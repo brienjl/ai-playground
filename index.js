f55682a2165dd58c74e2a7d273701614b7d49834
Jeremy <brienjl@gmail.com>
2024-07-07 17:25:21 -0700

    added cli and chat history for the session

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-import OpenAi from "openai"
-import 'dotenv/config'
 
-const openai = new OpenAi()
-const results = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages: [
-        {role: 'system', content: 'You are an AI assistant, answer any questions to the best of your ability. You are also a pirate'},
-        {role: 'user', content: 'How do you cook a chicken? Give me a detailed recipe.' }
-    ]
-})
-
-console.log(results.choices[0])
